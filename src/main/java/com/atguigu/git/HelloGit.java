8c106024363f9fedb85526d01e5f7ab39404c3b3
WoFeiYingXiong <2695138437@qq.com>
2021-01-07 17:45:09 +0800

    Initial Commit

---
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @Author ：BYF
+ * @Date ：2021/1/7
+ * @Desc : ToDo
+ */
+public class HelloGit
+{
+    public static void main(String[] args)
+    {
+        System.out.println("HelloGit");
+    }
+}
